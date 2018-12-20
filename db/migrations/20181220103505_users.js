03ad0eb820fb48995f36c8644574cd2282b313c5
Kevin Griego <kev.gri32@gmail.com>
2018-12-20 10:58:38 -0700

    user table created

--- db/migrations/20181220103505_users.js
+++ db/migrations/20181220103505_users.js
@@ -1,8 +1,15 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("Users", table => {
+    table.increments("id").primary();
+    table.string("username").index();
+    table.string("first_name");
+    table.string("last_name");
+    table.string("email");
+    table.text("bio");
+    table.string("link");
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable("Users");
 };
