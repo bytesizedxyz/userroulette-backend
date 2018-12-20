70348f1b86a5536f1e0aad20f0a44c717dbc26d8
David Kruse <thedavidkruse@gmail.com>
2018-12-20 12:30:41 -0700

    added redis service

--- services/redis.js
+++ services/redis.js
@@ -1,31 +1,54 @@
 const client = require('../config/redis');
 const knex = require('../db/knex');
 
-function addCachedUser(user){
-  let blah = client.set("featured", JSON.stringify(user), 'EX', 60 * 60 , (data) => {
-    return data;
-  });
+// documentation here: https://github.com/NodeRedis/node_redis
 
-  console.log(blah)
+
+// TODO:
+// post user into the caching solution.
+// current issue is that when retrieving the user it is null
+function addCachedUser(user) {
+  user = JSON.stringify(user)
+  let blah = client.set("featured", user, 'EX', 60 * 60, (err, ok) => {
+    console.log(err);
+    console.log(ok.toString())
+  });
   return blah
 }
 
-function getCachedUser(){
-  client.get("featured", (data) => {
-    if(data === null){
-      generateCachedUser()
-    }
+//TODO: retrieve from caching solution.
+// currently the data being retrieved is null, possible issue with storing.
+function getCachedUser() {
+  return new Promise((res, rej) => {
+    client.get("featured", (err, data) => {
+      if (err) {
+        console.log(err);
+        rej(err)
+      }
+      if (data === null) {
+        res(generateCachedUser())
+      } else {
+        console.log("kappa", data)
+        res(JSON.parse(data))
+      }
+    })
   })
+
 }
 
-function generateCachedUser(){
-  knex('users')
-  .select()
-  .then(data => {
-    console.log(data)
-  })
+async function generateCachedUser() {
+  let data = await knex('Users')
+    .select("id")
+    .catch(err => {
+      console.log(err)
+    })
+  
+  const newUserId = Math.floor(Math.random() * data.length) + 1  
+  const user = await knex("Users").select().where("id", newUserId).catch(err => console.log("error in generate cached user:", err))
+  addCachedUser(user)
+  return user;
 }
 
 module.exports = {
-  addCachedUser
+  getCachedUser
 }
--- services/users.js
+++ services/users.js
@@ -1,7 +1,16 @@
-const getUser = () => {
-
+const getUser = (username) => {
+  return new Promise((res, rej) => {
+    knex("Users")
+    .select("username", "first_name", "last_name", "link", "bio", "email")
+    .where("username", username)
+    .then(data => {
+      res(data)
+    })
+    .catch(err => {
+      rej(err)
+    })
+  })
 }
-
 module.exports = {
   getUser
 }
