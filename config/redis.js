ba600602e43e00a85f0efeee0fc9f2334a939e59
Kristian Freeman <kristian@kristianfreeman.com>
2018-12-21 13:35:29 -0700

    Use REDIS_URL if it exists

--- config/redis.js
+++ config/redis.js
@@ -1,9 +1,11 @@
 const redis = require("redis");
 
-const client = redis.createClient({
+const config = process.env.REDIS_URL || {
   port: process.env.REDIS_PORT,
   password: process.env.REDIS_PASSWORD,
   host: process.env.REDIS_HOST
-});
+};
+
+const client = redis.createClient(config);
 
 module.exports = client;
