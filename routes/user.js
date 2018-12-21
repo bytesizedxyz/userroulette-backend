99562e03dd1e9b4eed51f5a83af6bef3109284c6
Christopher White <christopher.evan.white88@gmail.com>
2018-12-21 11:13:47 -0700

    updated migration to add unique key constraint to username

--- routes/user.js
+++ routes/user.js
@@ -1,27 +1,33 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const knex = require("../db/knex");
-const { createUser, getUser } = require("../services/users");
-const { getCachedUser } = require("../services/redis");
+const knex = require('../db/knex');
+const { createUser, getUser } = require('../services/users');
+const { getCachedUser } = require('../services/redis');
 
-router.get("/featured", function(req, res, next) {
+router.get('/featured', function(req, res, next) {
   getCachedUser(req.body).then(data => {
     res.status(200).json(data);
   });
 });
 
-router.get('/:username', function(req, res, next) {
-  getUser(req.params.username)
-  .then(response=>{
-    res.json({ user:response });
-  })
-  .catch(err=>{
-    console.log(err);
-    res.status(400).json(err);
+router.get('/all', (req, res, next) => {
+  return knex('Users').then(response => {
+    res.json(response);
   });
 });
 
-router.post("/", function(req, res, next) {
+router.get('/:username', function(req, res, next) {
+  getUser(req.params.username)
+    .then(response => {
+      res.json({ user: response });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+router.post('/', function(req, res, next) {
   createUser(req.body)
     .then(response => {
       res.status(201).json({ user: response });
